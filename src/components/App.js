ff4937549c6f9771dd1b8025e3190478a96fa0e8
VUTNUR <vutnurnethajigoud@gmail.com>
2023-07-18 16:16:23 +0530

    assignment

--- src/components/App.js
+++ src/components/App.js
@@ -3,8 +3,14 @@ import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+   let [count, setCount]=useState(0);
+   function addCount(){
+        setCount(count+1)
+   }
   return (
     <div>
+      <p>Button clicked {count} times</p>
+      <button onClick={addCount}>Click me</button>
         {/* Do not remove the main div */}
     </div>
   )
